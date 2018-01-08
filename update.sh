1e8cf77615669491cae60766ca5f219a237cad7b
Stefan Meinecke <meinecke@greensec.de>
2018-01-08 23:17:58 +0000

    Fix update.sh
    Remove temp file on exit.

--- update.sh
+++ update.sh
@@ -3,14 +3,16 @@ cd disposable
 git pull >/dev/null
 cd ..
 
-tmpfile=$(mktemp)
 updated=0
+tmpfile=$(mktemp)
 ./disposable/.generate >$tmpfile && updated=1
 if [ "$updated" == "0" ]; then
     # no update
+    rm "$tmpfile"
     exit
 fi
 
-git commit -m "$(printf "Update domains\n\n"; cat $tmpfile)" domains.txt domains.json domains_sha1.json domains_sha1.txt
+git commit -m "$(printf "Update domains\n\n"; cat $tmpfile)" domains.txt domains.json domains_sha1.json domains_sha1.txt disposable
+rm "$tmpfile"
 git push
 
