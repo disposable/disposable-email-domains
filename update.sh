95cbb5380e28394a98d9f8eb2d1765b83283eadf
Stefan Meinecke <meinecke@greensec.de>
2023-10-29 19:28:02 +0100

    fix update.sh

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ git pull -q -f
 cd ..
 
 tmpfile=$(mktemp)
-./disposable/.generate --source-map --dns-verify >$tmpfile
+./disposable/.generate --source-map --dns-verify 2>$tmpfile
 git commit -m "$(printf "Update domains\n\n"; cat $tmpfile)" \
     domains.txt domains.json domains_legacy.txt domains_mx.txt domains_mx.json \
     domains_sha1.json domains_sha1.txt domains_source_map.txt \
