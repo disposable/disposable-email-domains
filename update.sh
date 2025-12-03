0900bfe6b63e45a53fdc2b6af12abc0c421e1848
Stefan Meinecke <meinecke@greensec.de>
2025-12-03 08:49:21 +0000

    update update.sh

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ cd ..
 
 tmpfile=$(mktemp)
 ./disposable/.generate --dedicated-strict --source-map --dns-verify 2>$tmpfile
-git commit -m "$(printf "Update domains\n\n"; cat $tmpfile)" \
+git commit -m "$(printf "Update domains\n\n"; head -n 500 $tmpfile)" \
     domains.txt domains.json domains_legacy.txt domains_mx.txt domains_mx.json \
     domains_sha1.json domains_sha1.txt domains_source_map.txt \
     domains_strict.json domains_strict.txt domains_strict_sha1.json domains_strict_sha1.txt \
